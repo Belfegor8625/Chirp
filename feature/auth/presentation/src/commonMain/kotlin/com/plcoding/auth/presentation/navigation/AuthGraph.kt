78a4fbab042e1676c30eaa24e62030a2dd1138a0
Bartosz Lewandowski <barlew94@o2.pl>
2026-09-21 14:37:33 +0200

    Deeplinking (part)

--- feature/auth/presentation/src/commonMain/kotlin/com/plcoding/auth/presentation/navigation/AuthGraph.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/plcoding/auth/presentation/navigation/AuthGraph.kt
@@ -3,7 +3,9 @@ package com.plcoding.auth.presentation.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.navigation.navDeepLink
 import androidx.navigation.navigation
+import com.plcoding.auth.presentation.email_verification.EmailVerificationRoot
 import com.plcoding.auth.presentation.register.RegisterRoot
 import com.plcoding.auth.presentation.register_success.RegisterSuccessRoot
 
@@ -15,12 +17,26 @@ fun NavGraphBuilder.authGraph(
         startDestination = AuthGraphRoutes.Register
     ) {
         composable<AuthGraphRoutes.Register> {
-            RegisterRoot(onRegisterSuccess = {
-                navController.navigate(AuthGraphRoutes.RegisterSuccess(it))
-            })
+            RegisterRoot(
+                onRegisterSuccess = {
+                    navController.navigate(AuthGraphRoutes.RegisterSuccess(it))
+                }
+            )
         }
         composable<AuthGraphRoutes.RegisterSuccess> {
             RegisterSuccessRoot()
+        }
+        composable<AuthGraphRoutes.EmailVerification>(
+            deepLinks = listOf(
+                navDeepLink {
+                    this.uriPattern = "https://chirp.pl-coding.com/api/auth/verify?token={token}"
+                },
+                navDeepLink {
+                    this.uriPattern = "chirp://chirp.pl-coding.com/api/auth/verify?token={token}"
+                },
+            )
+        ) {
+            EmailVerificationRoot()
         }
     }
 }
